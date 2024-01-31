d01796fc597fa7a12293e6559c4477465ff4a345
SurojuAkshayKumar <151859477+SurojuAkshayKumar@users.noreply.github.com>
2024-01-31 23:07:12 +0530

    Update Q1.cpp

--- Week 1/Q1.cpp
+++ Week 1/Q1.cpp
@@ -1,1 +1,40 @@
 #include<iostream>
+int ls(int arr[] ,int n,int key,int *comp) 
+{ 
+ (*comp)++ ;
+ if (arr[i] == key)
+   {
+    return i;
+   }
+}
+    return -1 ;
+}
+
+ int main() 
+ {
+  int n , key , comp = 0;
+  printf("Enter the size of the array:\n");
+   for(int i=0 ;i<n ; i++)
+    {
+     scanf(""%d" , &arr[i]);
+    }
+
+  printf("Enter the key element to search: ");
+  scanf("%d" , &key);
+
+int result = ls((arr , n , key , &comp);
+ if (result != -1)
+  {
+   printf("Element present %d \n" , result);
+  }
+else 
+ {
+  printf("Element not present \n");
+ }
+  printf ("Totak number of comparison : %d \n" , comp);
+
+  return 0 ;
+}
+
+
+
