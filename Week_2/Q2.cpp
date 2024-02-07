e06fa1541da4c1f1f51b6fb4191f8f8898234207
SurojuAkshayKumar <151859477+SurojuAkshayKumar@users.noreply.github.com>
2024-02-07 23:41:42 +0530

    Update Q2.cpp

--- Week_2/Q2.cpp
+++ Week_2/Q2.cpp
@@ -1,2 +1,44 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
+
+void findSequence(int arr[], int n) {
+    for (int k = 2; k < n; ++k) {
+        int left = 0;
+        int right = k - 1; 
+        while (left < right) {
+            if (arr[left] + arr[right] == arr[k])
+            {
+                cout << left << ", " << right << ", " << k << endl;
+                return;
+            } 
+            else if (arr[left] + arr[right] < arr[k]) 
+            {
+                left++;
+            } 
+            else 
+            {
+                right--;
+            }
+        }
+    }
+    cout << "No sequence found" << endl;
+}
+
+int main() {
+    int T, n;
+    cout << "Enter number of test cases: ";
+    cin >> T;
+    for (int t = 0; t < T; ++t) {
+        cout << "Enter size of array: ";
+        cin >> n;
+        int* arr = new int[n];
+        cout << "Enter array elements: ";
+        for (int i = 0; i < n; ++i)
+        {
+            cin >> arr[i];
+        }
+        findSequence(arr, n);
+        delete[] arr;
+    }
+    return 0;
+}
